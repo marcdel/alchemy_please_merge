b96eaf4b55f83a3598ee863373090c076a465ebe
Marc Delagrammatikas <delagrammatikas@gmail.com>
2020-06-26 09:19:58 -0700

    container is not used

--- content.js
+++ content.js
@@ -4,10 +4,6 @@ button.innerHTML = "Alchemy Please Merge"
 button.classList.add("btn")
 button.classList.add("btn-primary")
 
-const container = document.createElement("div")
-container.classList.add("bg-gray-light")
-container.appendChild(button)
-
 // 2. Place next to Comment
 const commentButton = document.evaluate("//button[contains(., 'Comment')]", document, null, XPathResult.ANY_TYPE, null).iterateNext()
 commentButton.parentNode.appendChild(button)
