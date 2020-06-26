75532212f2c09420d2bb1e1143e6a2a94570788c
Marc Delagrammatikas <delagrammatikas@gmail.com>
2020-06-26 12:39:12 -0700

    Check for approved changes

--- content.js
+++ content.js
@@ -1,25 +1,48 @@
-// 1. Create the button
-const button = document.createElement("button")
-button.innerHTML = "Alchemy Please Merge"
-button.classList.add("btn")
-button.classList.add("btn-primary")
+function addButtonToPage() {
+  // 1. Create the button
+  const button = document.createElement("button")
+  button.innerHTML = "Alchemy Please Merge"
+  button.id = "alchemy_merge_button"
+  button.classList.add("btn")
+  button.classList.add("btn-primary")
 
-// 2. Place next to Comment
-const commentButton = document.evaluate("//button[contains(., 'Comment')]", document, null, XPathResult.ANY_TYPE, null).iterateNext()
-commentButton.parentNode.appendChild(button)
+  // 2. Place next to Comment
+  const buttons = [].slice.call(document.querySelectorAll("button.btn-primary")).filter(button => button.textContent.match("Comment"))
+  const commentButton = buttons[buttons.length - 1]
+  commentButton.parentNode.appendChild(button)
 
-// 3. Add event handler
-button.addEventListener ("click", function(e) {
-  e.preventDefault()
-  const newCommentField = document.querySelector("#new_comment_field")
-  newCommentField.value = "alchemy please merge"
-  newCommentField.form.submit()
-  newCommentField.value = ""
-})
+  // 3. Add event handler
+  button.addEventListener("click", function (e) {
+    e.preventDefault()
+    const newCommentField = document.querySelector("#new_comment_field")
+    newCommentField.value = "alchemy please merge"
+    newCommentField.form.submit()
+    newCommentField.value = ""
+  })
 
-setInterval(function() {
-  const status = document.querySelector(".mergeability-details").querySelector(".status-meta")
-  const mergeable = status.innerText === "1 pending and 3 successful checks"
+  return button
+}
 
-  button.disabled = !mergeable
-},1000)
+function didPassCI() {
+  const status = document.evaluate("//span[contains(., '1 pending and 3 successful checks')]", document, null, XPathResult.ANY_TYPE, null).iterateNext()
+  return status.innerText.includes("1 pending and 3 successful checks")
+}
+
+function isApproved() {
+  const approval = document.querySelector(".mergeability-details").querySelector(".status-heading")
+  return approval.innerText.includes("Changes approved")
+}
+
+(function () {
+  const button = addButtonToPage()
+
+  setInterval(function () {
+    const approved = isApproved()
+    const passedCI = didPassCI()
+    const mergeable = passedCI && approved
+
+    // console.log(`PR mergeable? ${mergeable} - Passed CI? ${passedCI} / Approved? ${approved}`)
+
+    button.disabled = !mergeable
+  }, 1000)
+})()
