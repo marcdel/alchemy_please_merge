1378f01a81d43b40ee23a2df2b945d910e04f6aa
Marc Delagrammatikas <delagrammatikas@gmail.com>
2020-06-25 20:45:19 -0700

    Fixes double submit, button placement, and extension loading on non-github pr pages.

--- content.js
+++ content.js
@@ -9,12 +9,14 @@ container.classList.add("bg-gray-light")
 container.appendChild(button)
 
 // 2. Place next to Comment
-const buttonContainer = document.querySelector("#partial-new-comment-form-actions").querySelector(".d-flex .flex-justify-end")
-buttonContainer.appendChild(container)
+const commentButton = document.evaluate("//button[contains(., 'Comment')]", document, null, XPathResult.ANY_TYPE, null).iterateNext()
+commentButton.parentNode.appendChild(button)
 
 // 3. Add event handler
-button.addEventListener ("click", function() {
+button.addEventListener ("click", function(e) {
+  e.preventDefault()
   const newCommentField = document.querySelector("#new_comment_field")
   newCommentField.value = "alchemy please merge"
   newCommentField.form.submit()
+  newCommentField.value = ""
 })
